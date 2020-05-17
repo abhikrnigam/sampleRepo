22ca671e816f5a73ebe239314f92fb7834205851
Abhishek Kumar <abhikrnigam@gmail.com>
2020-05-18 00:34:05 +0530

    added a child in the container of cityScreen.dart

--- cityscreen.dart
+++ cityscreen.dart
@@ -9,6 +9,7 @@ class _CityScreenState extends State<CityScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
+	child:Text("This is some sample text in the container added on the dev branch")
 
     );
   }
